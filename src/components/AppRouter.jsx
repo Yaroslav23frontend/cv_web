6baf111aa4f9083b7cd7fccf1909ccd57f9cce71
Yaroslav Khomenko <kyaroslav180@gmail.com>
2022-06-24 19:00:57 +0300

    LCP optimization

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -14,33 +14,44 @@ import SignUp from "../pages/SignUp";
 import UserDeleted from "../pages/UserDeleted";
 import CurrentCV from "../pages/CurrentCV";
 import NotFound from "../pages/NotFound";
+import Container from "./Container";
+import { Circle } from "@react-pdf/renderer";
+import { CircularProgress } from "@material-ui/core";
 
 export default function AppRouter() {
-  const { isAuth } = useAuth();
+  const { isAuth, loading } = useAuth();
   console.log(isAuth);
   return (
     <Routes>
-      {isAuth ? (
-        <>
-          <Route path="/cv" element={<Collection />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/resetEmail" element={<ResetEmail />} />
-          <Route path="/resetPassword" element={<ResetPassword />} />
-          <Route exact path="/cv/:id" element={<CurrentCV />} />
-          <Route path="" element={<Navigate to="/cv" />} />
-        </>
-      ) : (
-        <>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/reset" element={<Reset />} />
-          <Route path="/resetMassege" element={<ResetRequest />} />
-          <Route path="/emailChanged" element={<EmailChangedSuccess />} />
-          <Route path="/passwordChanged" element={<PasswordChanged />} />
-          <Route path="/userDeleted" element={<UserDeleted />} />
-        </>
-      )}
-      <Route path="*" element={<NotFound />} />
+      <>
+        {loading === false ? (
+          <>
+            {isAuth ? (
+              <>
+                <Route index path="/cv" element={<Collection />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/resetEmail" element={<ResetEmail />} />
+                <Route path="/resetPassword" element={<ResetPassword />} />
+                <Route exact path="/cv/:id" element={<CurrentCV />} />
+                {/* <Route path="*" element={<Navigate to="/cv" />} /> */}
+              </>
+            ) : (
+              <>
+                <Route index path="/" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/reset" element={<Reset />} />
+                <Route path="/resetMassege" element={<ResetRequest />} />
+                <Route path="/emailChanged" element={<EmailChangedSuccess />} />
+                <Route path="/passwordChanged" element={<PasswordChanged />} />
+                <Route path="/userDeleted" element={<UserDeleted />} />
+              </>
+            )}
+            <Route path="*" element={<NotFound />} />
+          </>
+        ) : (
+          <></>
+        )}
+      </>
     </Routes>
   );
 }
